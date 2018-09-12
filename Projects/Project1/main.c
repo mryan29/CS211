67f09a6c2f8d413ec35b5adab32190c150634698
mryan29 <mryan29@nd.edu>
2018-09-12 15:05:33 -0500

    Finished Proj1

--- Projects/Project1/main.c
+++ Projects/Project1/main.c
@@ -1,92 +1,122 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+/* FUNCTION DECLARATIONS */
 void makeArrayCopy(int [], int [], int);
 void myFavoriteSort(int [], int);
-int linSearch (int [], int, int, int); // currently doesn't work searching on arr of less than 5
+int linSearch (int [], int, int, int); 
+int binSearch(int [], int, int, int*);
 void printArray(int [], int);
 
-/* current issues */
-// lin search doesnt work on arr of less than 5 values
-// not doing dynamic arrays
-// lin search needs to return two values, not one
-// lin search's last parameter should be int *, not just int
 int main (int argc, char** argv)
 {
- int val;
 
- /* prompt the user for input */
- printf ("Enter in a list of numbers to be stored in a dynamic array.\n");
- printf ("End the list with the terminal value of -999\n");
+ 	/* prompt the user for input */
+ 	system("clear");
+ 	printf ("\nEnter in a list of numbers to be stored in a dynamic array.\n");
+ 	printf ("End the list with the terminal value of -999\n");
  
- // initialize array to max size
- int firstArray[100];
- int size = 0;
+ 	// initialize array to max size
+ 	int val; 					// stores user input
+ 	int sz = 100;				// initial size of array
+ 	int size = 0;				// keep track of how many elements to store in array
+ 	int *firstArray = (int*) malloc(sizeof(int) * sz); 	// dynamically allocate array
  
- /* loop until the user enters -999 */
- scanf ("%d", &val);
- while (val != -999)
-   {
-    /* store the value into an array */
-    firstArray[size] = val;
+ 
+ 	/* loop until the user enters -999 */
+ 	scanf ("%d", &val);
+ 	if (val == -999) { printf("Invalid input. List must be of minimum size 1. Exiting program.\n"); return 0; }		// added to handle empty input
+ 	
+	while (val != -999) {
+		
+    	/* store the value into an array */
+    	if (size >= sz) {
+    		int* tmp = (int *) malloc(sizeof(int) * (sz * 2));
+    		for (int i = 0; i < sz; i++) {
+    			tmp[i] = firstArray[i];
+    		}
+    		free(firstArray);
+    		firstArray = tmp;
+    		sz = sz * 2;
+    	}
+    	firstArray[size] = val;
 
-    /* get next value */
-    scanf("%d", &val);
+   		/* get next value */
+    	scanf("%d", &val);
     
-    size++;
+    	size++;
    }
 
- /* make a copy of the array of values */
-	//secondArray = malloc(sizeof(int) * size);
-	int secondArray[size];
+ 	/* make a copy of the array of values */
+	int* secondArray = (int*) malloc(sizeof(int) * size); // size == sz???
 	makeArrayCopy(firstArray, secondArray, size);
 		
-		
 	// testing
+	printf("\nFirst Array:\t\t\t");
 	for (int i = 0; i < size; i++) {
 		printf("%d ", firstArray[i]);
 	}
 	printf("\n");
+	printf("Second Array (Copied):\t\t");
 	for (int i = 0; i < size; i++) {
 		printf("%d ", secondArray[i]);
 	}
-	printf("\n");
+	printf("\n"); 
  
- /* sort one of the arrays */
- myFavoriteSort(secondArray, size);
- printArray(secondArray, size);
+ 	/* sort one of the arrays */
+	myFavoriteSort(secondArray, size);
+ 	printf("Second Array (Sorted):\t\t");
+ 	printArray(secondArray, size);
+ 	printf("\n\n");
  
 
- /* loop until the user enters -999 */
- printf ("Enter in a list of numbers to use for searching.  \n");
- printf ("End the list with a terminal value of -999\n");
- scanf ("%d", &val);
- int found;
- while (val != -999)
-   {
-   	printf("val: %d\n", val);
-    /* use the value in a linear search on the unsorted array */
-    /* print out info about the linear search results  */
-	found = linSearch(firstArray, size, val, 0);
-	if (found == -1) {
-		printf("Target not found at position: -1,\t number of comparisons: %d.\n", size);
-	} else {
-		printf("Found target with numComparisons: %d, at pos: %d\n", found, found-1);
+ 	/* loop until the user enters -999 */
+ 	printf ("Enter in a list of numbers to use for searching.  \n");
+ 	printf ("End the list with a terminal value of -999\n");
+ 	scanf ("%d", &val);
+ 	if (val == -999) { printf("Invalid input. List must be of minimum size 1. Exiting program.\n"); return 0; }		// added to handle empty input
+ 
+ 	while (val != -999)
+	{
+		int linPos = 0, linRes = 0;
+		int binPos = 0, binRes = 0;
+		
+   		printf("\nTarget: %d\n", val);
+   		
+    	/* use the value in a linear search on the unsorted array */
+    	printf("*Linear Search*\n");
+		linPos = linSearch(firstArray, size, val, 0);
+		
+		/* print out info about the linear search results  */
+		if (linPos == -1) {
+			printf("Target not found at position: -1,\t number of comparisons: %d.\n", size);
+		} else {
+			printf("Found target with numComparisons: %d, at pos: %d\n", linPos+1, linPos);
+		}
+	
+    	/* use the value in a binary search on the sorted array */
+		printf("*Binary Search*\n");
+		binRes = binSearch(secondArray, size, val, &binPos);
+		
+		/* print out info about the binary search results  */
+		if (binRes == -1) {
+			printf("Target not found at position: -1,\t number of comparisons: %d.\n\n", size);
+		} else {
+			printf("Found target with numComparisons: %d, at pos: %d\n\n", binRes+1, binRes);
+		}
+		
+    	/* get next value */
+   		scanf("%d", &val);
 	}
-    /* use the value in a binary search on the unsorted array */
 
-    /* print out info about the binary search results  */
-
-    /* get next value */
-    scanf("%d", &val);
-   }
+	printf ("Good bye\n");
+ 	return 0;
+ } 
 
 
+/* FUNCTION DEFINITIONS */
 
- printf ("Good bye\n");
- return 0;
-} 
-
-
+/* makeArrayCopy */
 void makeArrayCopy(int fromArray[], int toArray[], int size) {
 	for (int i = 0; i < size; i++) {
 		//*(toArray + i) = fromArray[i];
@@ -94,8 +124,8 @@ void makeArrayCopy(int fromArray[], int toArray[], int size) {
 	}
 }
 
+/* myFavoriteSort */
 void myFavoriteSort(int arr[], int size) {
-
 	for (int j = 0; j < size - 1; j++) {
 		for (int i = 0; i < size - j - 1; i++) {
 			if (arr[i] > arr[i+1]) {
@@ -105,21 +135,50 @@ void myFavoriteSort(int arr[], int size) {
 			} 
 		}
 	}
-
 }
 
+/* linSearch */
 int linSearch (int arr[], int size, int target, int numComparisons) {
+	
 	for (numComparisons = 0; numComparisons < size; numComparisons++) {
+		
 		if (arr[numComparisons] == target) {break;
 		} else if (arr[numComparisons] != target && numComparisons == size - 1) {
 			numComparisons = -1;break;
 		}
-		numComparisons++;
+		
 	}
+	
 	return numComparisons;
-
 }
 
+/* binSearch */
+int binSearch(int arr[], int size, int target, int *numComparisons) {
+	
+	int lower = 0; 			// lower bound (first element)
+	int upper = size - 2;	// upper bound (last element)
+	
+	
+	while (lower <= upper) {
+	
+		int midpt = (lower + upper)/2;		// midpoint of array
+		*numComparisons++;	
+		
+		if (target > arr[midpt]) { lower = midpt + 1; }		// search upper half of array
+		
+		else if (target == arr[midpt]) {					// return midpoint if equal
+			return midpt;
+			break;
+		} 
+		
+		else { upper = midpt - 1; }							// search lower half of array
+	}
+	
+	if (lower > upper) { return -1; } 						// if not found, return -1
+	
+}
+
+/* helper print function */
 void printArray(int arr[], int size) {
 	for (int i = 0; i < size; i++) {
 		printf("%d ", arr[i]);
